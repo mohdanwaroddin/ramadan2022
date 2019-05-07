52afa4ca7042031a3564d5de1835e23f6cb4e8c8
Sabbir Ahmed <sabbirs93@gmail.com>
2019-05-07 16:22:53 +0600

    Add files via upload

--- assets/js/style.js
+++ assets/js/style.js
@@ -37,4 +37,16 @@ swal({
     button: "Thanks!",
  });
 
+ var n = localStorage.getItem('on_load_counter');
+
+ if (n === null) {
+     n = 0;
+ }
+
+ n++;
+
+ localStorage.setItem("on_load_counter", n);
+
+ document.getElementById('CounterVisitor').innerHTML = n;
+
  /* All Rights reserved by Sabbir */
